cb2894f3dfa1c286c67922562979149b4d8016ac
rosh789 <rrockyupadhyay789@gmail.com>
2025-04-21 14:08:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -32,3 +32,21 @@ def main():
 
 if __name__ == '__main__':
     main()
+import os
+from telegram.ext import Updater, CommandHandler
+
+def start(update, context):
+    update.message.reply_text('Hello! I am your Aviator Predictor Bot.')
+
+def main():
+    token = os.getenv("BOT_TOKEN")
+    updater = Updater(token, use_context=True)
+
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start", start))
+
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
